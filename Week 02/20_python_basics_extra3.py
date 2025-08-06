997e710744b7f6da7640427ddca025acfc52ac3c
Benjamin <ben@MacBook-Air-von-Benjamin.local>
2025-08-06 14:15:27 +0200

    finished extra3 file

--- Week 02/20_python_basics_extra3.py
+++ Week 02/20_python_basics_extra3.py
@@ -2,18 +2,25 @@
 
 #Starting money
 wallet = 20.0
-
+print("Starting wallet:", wallet)
 
 #1. You drunk a coffee today that costed 3.45€. Subtract coffee cost from wallet using -=
-
+wallet -= 3.45
+print ("Actual wallet, drunk coffee:", wallet)
 
 #2. Someone gave you a present today. Add a gift card value of $10 using +=
+wallet += 10
+print ("Actual wallet, got giftcard:", wallet)
+
 
 #3. You were lucky and found some money in your pockets. Double the value in the wallet using *=
+wallet *= 2
+print ("Actual wallet, found money:", wallet)
 
 
 #4. The book you were waiting for came out. Subtract the cost from your wallet as in step 1 -=
-
+wallet -= 19.90
+print ("Actual wallet, bought book:", wallet)
 
 
 
