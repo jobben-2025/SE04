dab12ee8b0ecbfa567206301b2925977a129f5d3
Benjamin <ben@MacBook-Air-von-Benjamin.local>
2025-08-06 14:09:35 +0200

    finished basics2 file

--- Week 02/19_python_basics_extra2.py
+++ Week 02/19_python_basics_extra2.py
@@ -9,23 +9,33 @@ cookie_price = 1.25
 
 #Number of items a customer wants
 coffees = 2
-sandwwiches = 1
+sandwhiches = 1
 cookies = 3
 
 
 #1. Calculate the total cost of all items purchased; use + and *
+coffee_sold = coffee_price * coffees
+sandwhich_sold = sandwhich_price * sandwhiches
+cookie_sold = cookie_price * cookies
+total = coffee_sold + sandwhich_sold + cookie_sold
+print(f"Total cost of all sold items purchased is {total}")
 
 
 #2. Calculate the average price of the items bought; use / and ()
+average_price_items = total / (coffees+sandwhiches+cookies)
+print("Average item price derived (total cost / total amount sold):", average_price_items)
 
-
-
-#3. Check how many cookies one can buy with $10; use //
-
+#3. Check how many cookies one can buy with $10; use // ###the floor division // rounds the result down to the nearest whole number
+given_money = 10
+affordable_amount_cookies = given_money // cookie_price
+print("With 10$ you can afford (cookies):", affordable_amount_cookies)
 
 #4. Find the leftover money if a customer buys as many cookies as possible with $10; use %
+change_money = given_money - (affordable_amount_cookies * cookie_price)
+print("You receive change:", change_money)
 
-
+#check_amount = affordable_amount_cookies*cookie_price
+#print(check_amount)                    #8*1.25=10
 
 
 
